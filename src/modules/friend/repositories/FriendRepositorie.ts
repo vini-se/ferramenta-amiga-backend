c35309c4dd079b037d1d207aee3f4a0b5d11d405
Vinicius <vi058590@intelbras.com.br>
2023-07-11 12:13:54 -0300

    chore: create friend prisma repositorie

---
+++ src/modules/friend/repositories/FriendRepositorie.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from "@prisma/client";
+import { IFriendDTO } from "./dto/IFriendDTO";
+
+class FriendRepositorie {
+  private readonly prisma: PrismaClient;
+
+  constructor() {
+    this.prisma = new PrismaClient();
+  }
+
+  public async createFriend(data: IFriendDTO) {
+    try {
+      const friend = await this.prisma.friend.create({
+        data: data
+      });
+      console.log(`O amigo ${friend.name} foi criado com sucesso!`)
+      return friend;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+}
