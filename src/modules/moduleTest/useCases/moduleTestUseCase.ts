cf8dbfcc7eb02408d1eb579b131997992d2fbdc6
Vinicius <vi058590@intelbras.com.br>
2023-07-11 11:39:40 -0300

    BaseFIle: create test UseCase implementing test repositorie

--- src/modules/moduleTest/useCases/moduleTestUseCase.ts
+++ src/modules/moduleTest/useCases/moduleTestUseCase.ts
@@ -8,8 +8,30 @@ class ModuleTestUseCase {
         this.testRepositorie = new TestRepositorie();
     }
 
-    public async create(data: ITestData) {
-        const test = await this.testRepositorie.createTest(data);
+    // public async create(data: ITestData) {
+        // const test = await this.testRepositorie.createTest(data);
+    public async create() {
+        const test = await this.testRepositorie.createTest();
+        return test;
+    }
+
+    public async getAll(): Promise<ITestData[]> {
+        const tests = await this.testRepositorie.getTests();
+        return tests;
+    }
+
+    public async getById(id: number): Promise<ITestData | null> {
+        const test = await this.testRepositorie.getTestById(id);
+        return test;
+    }
+
+    public async update(data: ITestData): Promise<ITestData | null> {
+        const test = await this.testRepositorie.updateTest(data);
+        return test;
+    }
+
+    public async delete(id: number): Promise<boolean> {
+        const test = await this.testRepositorie.deleteTest(id);
         return test;
     }
 
