83ebb580e771e0810c4d93847740c623ad18449b
Vinicius <vi058590@intelbras.com.br>
2023-07-11 11:35:20 -0300

    BaseFile: prisma test controller

--- src/modules/moduleTest/repositories/testRepositorie.ts
+++ src/modules/moduleTest/repositories/testRepositorie.ts
@@ -4,17 +4,68 @@ const prisma = new PrismaClient();
 
 class TestRepositorie {
 
-  public async createTest(data: ITestData) {
+  public async createTest() {
     try {
-      const test = await prisma.teste.create({
-        data: {
-          id: data.id,
-          created_at: data.createdAt
+      const test = await prisma.teste.create({});
+      console.log(`O teste ${test.id} foi criado com sucesso!`)
+      return test;
+    }catch (e){
+      console.log(e);
+    }
+  }
+
+  public async getTests(): Promise<ITestData[]>  {
+    try {
+      const test = await prisma.teste.findMany();
+      return test;
+    }catch (e){
+      console.log(e);
+      return []
+    }
+  }
+
+  public async getTestById(id: number): Promise<ITestData | null>  {
+    try {
+      const test = await prisma.teste.findFirst({
+        where: {
+          id: id
         }
       });
       return test;
     }catch (e){
       console.log(e);
+      return null
+    }
+  }
+
+  public async updateTest(data: ITestData): Promise<ITestData | null>  {
+    try {
+      const test = await prisma.teste.update({
+        where: {
+          id: data.id
+        },
+        data: data,
+      });
+      console.log(`O teste ${data.id} foi atualizado com sucesso!`)
+      return test;
+    }catch (e){
+      console.log(e);
+      return null
+    }
+  }
+
+  public async deleteTest(id: number): Promise<boolean>  {
+    try {
+      const test = await prisma.teste.delete({
+        where: {
+          id: id
+        }
+      });
+      console.log(`O teste ${id} foi deletado com sucesso!`)
+      return true;
+    }catch (e){
+      console.log(e);
+      return false
     }
   }
 
