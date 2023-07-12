992be225a48c616783dfb4cb42d7f99cf71533f5
Vinicius <vi058590@intelbras.com.br>
2023-07-12 10:40:36 -0300

    chore: add tool routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { FriendRoutes } from "./friend.routes";
+import { ToolRoutes } from "./tool.routes";
 
 const router = Router();
 
 router.use("/", FriendRoutes)
+router.use("/", ToolRoutes)
 
 export { router };
