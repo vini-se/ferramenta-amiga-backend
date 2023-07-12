aefe1d2bcbac99b0f91e63961e6ab2a44d7c90c4
Vinicius <vi058590@intelbras.com.br>
2023-07-11 23:37:26 -0300

    chore: implement friend routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { TestRoutes } from "./test.routes";
+import { FriendRoutes } from "./friend.routes";
 
 const router = Router();
 
 router.use("/", TestRoutes)
+router.use("/", FriendRoutes)
 
 export { router };
